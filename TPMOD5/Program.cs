ea50578db7a9988d4ce0459a98d7d7bdd039902d
xxraihann <raihanananda1980@gmail.com>
2025-03-16 16:14:54 +0700

    “menambahkan class DataGeneric

--- TPMOD5/Program.cs
+++ TPMOD5/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main()
     {
         HaloGeneric.SapaUser("Raihan");
+
+        DataGeneric<string> nimData = new DataGeneric<string>("103022330075");
+        nimData.PrintData();
+
     }
 }
